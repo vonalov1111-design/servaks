46525b979e9aa0e09201828ec5c418ab39b32e29
vonalov1111-design <vonalov1111@gmail.com>
2025-10-24 17:17:11 +0300

    Add files via upload

---
+++ start.bat
@@ -0,0 +1,9 @@
+@echo off
+chcp 65001 > nul
+title Servaks RP Server
+echo ===============================
+echo    Starting Servaks RP Server
+echo ===============================
+cd /d "C:\Users\User\Desktop\servaks"
+server.exe
+pause
